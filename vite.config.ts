6d4a51ae38349c1f1490c20ce3c5751f7872344f
Dylan Kwok <flyingpig071@gmail.com>
2026-05-26 10:02:42 -0700

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/The-Sands/', // add this
+    base: '/', // add this
 
     plugins: [react(), tailwindcss()],
 
